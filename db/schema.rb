5d69d003ce725c6c96278fdfc5f97082befcab52
chuanqizhu <chuanqizhu1991@outlook.com>
2021-07-21 18:05:43 +0800

    generate user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_21_094828) do
+ActiveRecord::Schema.define(version: 2021_07_21_100322) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.datetime "time"
     t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
